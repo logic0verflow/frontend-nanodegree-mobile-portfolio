7d8fdf50f2e30d39fe79aff147b3228130252c62
logic0verflow <git@nathanacosta.com>
2017-08-13 23:01:01 -0600

    Optimize random pizza generation

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -465,11 +465,16 @@ var resizePizzas = function(size) {
 
 window.performance.mark("mark_start_generating"); // collect timing data
 
-// This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
-  pizzasDiv.appendChild(pizzaElementGenerator(i));
-}
+
+(function() {
+
+	// This for-loop actually creates and appends all of the pizzas when the page loads
+	var pizzasDiv = document.getElementById("randomPizzas");
+	for (var i = 2; i < 100; i++) {
+	  pizzasDiv.appendChild(pizzaElementGenerator(i));
+	}
+
+})();
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
 window.performance.mark("mark_end_generating");
